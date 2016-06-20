fd454dbc0f7108cb6274ac51079908a2b1788840
Jorge Rubio <jorgerc85@gmail.com>
2016-06-20 09:42:13 -0400

    TwitterImgToolbar: Add logic to callback to define when to add the toolbar

--- twitterImgToolbar.js
+++ twitterImgToolbar.js
@@ -6,6 +6,15 @@ function observeGalleryDiv() {
   var galleryDiv = document.getElementsByClassName('Gallery-media')[0];
   var observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
+      var childListMutation = (mutation.type === 'childList');
+      var mutationAddsChild = (mutation.addedNodes.length > 0);
+      if (childListMutation && mutationAddsChild) {
+        var addedChild = mutation.addedNodes[0];
+        var childAddedIsImg = (addedChild.tagName === 'IMG');
+        if (childAddedIsImg) {
+          // Add toolbar
+        };
+      };
     });
   });
   var config = { subtree: true, childList: true };
